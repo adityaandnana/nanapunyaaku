883848735eb617676edef38c879ce857db309512
adityaandnana <148787716+adityaandnana@users.noreply.github.com>
2023-10-24 23:45:32 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function clicks() {
+    $('#div1').hide();
+    $('#div2').show();
+    $('#div2').addClass("fadeinv2");
+        
+    var typed = new Typed('.katakata', {
+        strings: ["Cuma mau bilang", "Selamat Pagi Sayangku", "Aku Sayang Banget Sama Kamu", "I Love You Nana Cantik"],
+        typeSpeed: 70
+    });
+}
